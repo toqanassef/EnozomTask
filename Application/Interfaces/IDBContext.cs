b2c6169e7a66bd9067956c910084b4addc22ba8e
Toqa Nassef <toqanassef@gmail.com>
2021-12-20 12:39:57 +0200

    DB& replace Configration

---
+++ Application/Interfaces/IDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IDBContext
+    {
+        DbSet<Country> Countries { get; set; }
+        DbSet<CountryHolidays> CountryHolidays { get; set; }
+        DatabaseFacade Database { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Persistance/DBContext.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application
+{
+    public partial class DBContext : DbContext, IDBContext
+    {
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<CountryHolidays> CountryHolidays { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DbContext).Assembly);
+        }
+        public override int SaveChanges()
+        {
+            // https://stackoverflow.com/questions/26908301/best-way-to-override-savechanges/26908556
+            //https://stackoverflow.com/questions/60909477/ef-core-3-has-value-generator           
+            return base.SaveChanges();
+        }
+    }
+}
