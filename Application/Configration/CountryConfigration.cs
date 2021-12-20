62ec75db1cd1a37b05644a80af893f7bbcf3296d
Toqa Nassef <toqanassef@gmail.com>
2021-12-20 12:19:37 +0200

    AddConfigrations

---
+++ Application/Configration/CountryConfigration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Configration
+{
+    public class CountryConfigration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            //builder.ToTable("Countries");
+            builder.Property(x => x.Id)
+                .IsRequired();
+            builder.Property(at => at.Name)
+                .HasMaxLength(500).IsRequired();
+           
+        }
+    }
+}
---
+++ Application/Configration/CountryHolidaysConfigration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Configration
+{
+    public class CountryHolidaysConfigration : IEntityTypeConfiguration<CountryHolidays>
+    {
+        public void Configure(EntityTypeBuilder<CountryHolidays> builder)
+        {
+            //builder.ToTable("Countries");
+            builder.Property(x => x.Id)
+                .IsRequired();
+            builder.Property(x => x.CountryId)
+                .IsRequired();
+            builder.Property(at => at.Holiday)
+                .HasMaxLength(500).IsRequired();
+           
+        }
+    }
+}
