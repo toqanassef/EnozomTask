dec5d6179caeb96e4c124f7350c7969ba3001711
Toqa Nassef <toqanassef@gmail.com>
2021-12-20 12:02:17 +0200

    Add project files.

---
+++ Domain/Entities/CountryHolidays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CountryHolidays
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public string Holiday { get; set; }
+    }
+}
